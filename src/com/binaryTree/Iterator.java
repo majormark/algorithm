5ce2f529849b9d42b7d5d7e0df4a5268a6914e6e
majormark <406596504@qq.com>
2016-08-23 21:05:21 +0800

    二叉树遍历总结

--- src/com/binaryTree/Iterator.java
+++ src/com/binaryTree/Iterator.java
@@ -1,9 +1,17 @@
 package com.binaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Created by major on 16/8/22.
+ *
+ * 二叉树的几种遍历方式
  */
 public class Iterator {
+
+    //递归 先序遍历
     public void preOrderRecursion(TreeNode root) {
         if(root != null) {
             System.out.println(root.val);
@@ -12,6 +20,7 @@ public class Iterator {
         }
     }
 
+    //递归 中序遍历
     public void inOrderRecursion(TreeNode root) {
         if(root != null) {
             preOrderRecursion(root.left);
@@ -20,6 +29,7 @@ public class Iterator {
         }
     }
 
+    //递归 后序遍历
     public void postOrderRecursion(TreeNode root) {
         if(root != null) {
             preOrderRecursion(root.left);
@@ -28,7 +38,99 @@ public class Iterator {
         }
     }
 
+    /**
+     *
+     * @param root
+     *
+     * 非递归 先序遍历
+     * 先遍历左子树,先读取值然后后压栈,直到遇到null以后,出栈,得到右子树,继续之前的遍历
+     */
     public void preOrderIteration(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while(node != null || stack.size() > 0) {
+            while(node != null) {
+                System.out.println(node.val);
+                stack.push(node);
+                node = node.left;
+            }
 
+            if(stack.size() > 0) {
+                node = stack.pop();
+                node = node.right;
+            }
+
+        }
     }
+
+    /**
+     *
+     * @param root
+     *
+     * 非递归 中序遍历
+     * 类似于先序遍历,调整压栈和打印的位置
+     * 先遍历左子树,然后遇到null,出栈,打印值,得到右子树,重复之前的步骤
+     */
+    public void inOrderIteration(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node != null || stack.size() > 0) {
+            while(node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            if(stack.size() > 0) {
+                node = stack.pop();
+                System.out.println(node.val);
+                node = node.right;
+            }
+        }
+    }
+
+    /**
+     *
+     * @param root
+     *
+     * 非递归 后序遍历
+     * 使用一个结点记录前一个输出
+     * 同样先遍历左子树,直到遇到null,打印该结点,取栈顶元素的右节点但该元素不出栈,只有当右节点为空或等于前一个输出时,才打印该节点
+     */
+    public void postOrderIteration(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        TreeNode prev = node;
+        while(node != null || stack.size() > 0) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            if(stack.size() > 0) {
+                node = stack.peek().right;
+                if(node == null || node == prev) {
+                    node = stack.pop();
+                    System.out.println(node.val);
+                    prev = node;
+                    node = stack.peek().right;
+                }
+            }
+        }
+    }
+
+    public void levelIteration(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode node = root;
+        node = root;
+        queue.add(node);
+        while(queue.size() > 0) {
+            node = queue.poll();
+            System.out.println(node.val);
+            if(node.left != null) {
+                queue.offer(node.left);
+            }
+            if(node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+    }
+
 }
--- src/com/string/IdenticalTree.java
+++ src/com/string/IdenticalTree.java
@@ -6,6 +6,12 @@ import java.util.Stack;
 
 /**
  * Created by major on 16/8/22.
+ *
+ * 对于两棵彼此独立的二叉树A和B，请编写一个高效算法，检查A中是否存在一棵子树与B树的拓扑结构完全相同。
+ * 给定两棵二叉树的头结点A和B，请返回一个bool值，代表A中是否存在一棵同构于B的子树。
+ *
+ * 思路:最优解 使用二叉树序列化的方式,将两棵二叉树序列化为字符串,然后使用kmp算法验证是否包含那棵二叉树
+ * 复杂度 O()
  */
 
 
@@ -23,6 +29,7 @@ public class IdenticalTree {
         return false;
     }
 
+    //二叉树的
     public String serialize(TreeNode root) {
         if(root == null) {
             return null;
